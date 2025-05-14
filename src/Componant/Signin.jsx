982199ff5bee29a6187003687718312135fe8960
Your Name <your.email@example.com>
2025-05-13 21:51:49 -1200

    1st commit

--- src/Componant/Signin.jsx
+++ src/Componant/Signin.jsx
@@ -16,13 +16,13 @@
   return (
     <div>
       <form onSubmit={handleSubmit(submitHandler)} className='h-100vh p-20'>
-          <h1 className="text-3xl font-bold p-10 "> Get back to them</h1>
+          <h1 className="text-3xl font-bold p-10 "> Get back to them...</h1>
           
-          <input {...register("Email")} className='outline-0 border-0 font-light text-2xl text-black' type="Email" placeholder='enter email' /><hr />
-          <input {...register("password")} className='outline-0 border-0 font-light text-2xl text-black' type="password" placeholder='********' /><hr />
-          <button className='rounded-2xl p-2 mt-2 bg-blue-700' type='submit'>signUP</button><br />
+          <input {...register("Email")} className='outline-0 border-0 font-light text-2xl text-black ' type="Email" placeholder='enter email' required/><hr />
+          <input {...register("password")} className='outline-0 border-0 font-light text-2xl text-black ' type="password" placeholder='********' required/><hr />
+          <button className='rounded-2xl p-2 mt-2 bg-blue-700' type='submit'>signIn</button><br />
           <small className="text-xl ">don't have an account?
-          <button type="button" className='text-color-blue-500 font-bold ml-2  text-blue-700' onClick={()=>{setToggler(!toggler)}}>signIn</button></small>
+          <button type="button" className='text-color-blue-500 font-bold ml-2  text-blue-700' onClick={()=>{setToggler(!toggler)}}>signUP</button></small>
         </form>
     </div>
   )
--- src/Componant/Signup.jsx
+++ src/Componant/Signup.jsx
@@ -13,24 +13,24 @@ import { useForm } from 'react-hook-form';
       }
       const alreadyExists = users.some((users)=> users.Email === data.Email);
       if(alreadyExists){
-        alert("❗❗❗already registered");
+        alert("❗already signIN❗");
         return;
       }
       data.id = nanoid();
       const updateUsers = [...users, data];
       setUsers(updateUsers);
 
-      alert("successful✅");
-     reset()
-        }
+      alert("signUP successfully✅");
+      reset()
+      }
        
   return (
     <div>
       <form onSubmit={handleSubmit(submitHandler)} className='h-100vh p-20'>
           <h1 className='text-4xl p-10 font-bold'>Hello let's go to the shope</h1>
-          <input {...register("name")} className='outline-0 border-0 text-2xl text-black font-light' type="text" placeholder='enter your name' /><hr />
-          <input {...register("Email")} className='outline-0 border-0 text-2xl text-black font-light' type="Email" placeholder='enter email' /><hr />
-          <input {...register("password")} className='outline-0 border-0 text-2xl text-black font-light' type="password" placeholder='********' /><hr />
+          <input {...register("name")} className='outline-0 border-0 text-2xl text-black font-light' type="text" placeholder='enter your name' required/><hr />
+          <input {...register("Email")} className='outline-0 border-0 text-2xl text-black font-light' type="Email" placeholder='enter email' required/><hr />
+          <input {...register("password")} className='outline-0 border-0 text-2xl text-black font-light' type="password" placeholder='********' required/><hr />
           <button className='rounded-2xl mt-2 p-2 text-2xl bg-blue-700  ' type='submit'>signUP</button><br />
           <small className='text-xl '>already have an account?
           <button className=' text-blue-700 font-bold ml-2' type='button' onClick={()=>{setToggler(!toggler)}}>signIn</button></small>
